ef04fc13bff0366ba2f3789df16a42e5a029f413
Aminu Rabiu <arabiu033@gmail.com>
2022-12-14 17:01:53 +0100

    Fix the code bugs

--- morse.rb
+++ morse.rb
@@ -1,22 +1,25 @@
 def decode_char(char)
   hash = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
            '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-           '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
+           '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
            '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3',
            '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7',
            '---..' => '8', '----.' => '9', '-----' => '0' }
-  print(hash[char])
+   return (hash[char])
 end
 
 def decode_word(string)
+  str = ""
   ar = string.split(/ /)
-  ar.each { |value| decode_char(value) }
-  print(' ')
+  ar.each { |value| str += decode_char(value) }
+  return str
 end
 
 def decode(string)
+  str = ""
   ar = string.split(/   /)
-  ar.each { |value| decode_word(value) }
+  ar.each { |value| str += decode_word(value) + " " }
+  return str
 end
 
 decode('-- -.--   -. .- -- .')
